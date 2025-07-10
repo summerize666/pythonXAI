fd31dea5d1f3d854b45503d458e4a8447f64ef7c
summerize666 <proodavio123@gmail.com>
2025-07-10 08:35:47 +0800

    class3

--- pages/class3-3.py
+++ pages/class3-3.py
@@ -1,16 +1,16 @@
-print([]) # 這是一個空的 list
-print([1, 2, 3]) # 這是一個有三個元素的 list
-print([1, 2, 3, "a", "b", "c"]) # 這是一個有六個元素的 list
-print([1, 2, 3, ["a", "b", "c"]]) # 這是一個有四個元素的 list
-print([1, True, "a", 1.23]) # 這是一個有四個元素的 list
+print([])  # 這是一個空的 list
+print([1, 2, 3])  # 這是一個有三個元素的 list
+print([1, 2, 3, "a", "b", "c"])  # 這是一個有六個元素的 list
+print([1, 2, 3, ["a", "b", "c"]])  # 這是一個有四個元素的 list
+print([1, True, "a", 1.23])  # 這是一個有四個元素的 list
 
 # list 讀取元素，元素的 index 從 0 開始
 
 L = [1, 2, 3, "a", "b", "c"]
-print(L[0]) # 1
-print(L[1]) # 2
-print(L[2]) # 3
-print(L[3]) # "a"
+print(L[0])  # 1
+print(L[1])  # 2
+print(L[2])  # 3
+print(L[3])  # "a"
 
 L = [1, 2, 3, "a", "b", "c"]
 
@@ -31,7 +31,7 @@ print(L[1:4:2])
 # list 取長度，也就是 list 中有幾個元素，不是 index 的最大值
 
 L = [1, 2, 3, "a", "b", "c"]
-print(len(L)) # 6
+print(len(L))  # 6
 
 # list 走訪元素
 
@@ -43,42 +43,42 @@ print(len(L)) # 6
 
 L = [1, 2, 3, "a", "b", "c"]
 for i in range(0, len(L), 2):
-print(L[i])
+    print(L[i])
 
 for i in L:
-print(i)
+    print(i)
 
 # list 修改元素
 
 # 可以透過 index 來修改 list 中的元素
 
 L = [1, 2, 3, "a", "b", "c"]
-L[0] = 2 # 把 index 0 的元素改成 2
+L[0] = 2  # 把 index 0 的元素改成 2
 print(L)
 
 # call by value
 
 a = 1
-b = a # 複製 a 的值給 b
+b = a  # 複製 a 的值給 b
 b = 2
-print(a, b) # a 還是 1，b 變成 2
+print(a, b)  # a 還是 1，b 變成 2
 
 # call by reference
 
 a = [1, 2, 3]
-b = a # 把 a 跟 b 指向同一個記憶體位置，所以改變 b 的值，a 也會跟著改變
+b = a  # 把 a 跟 b 指向同一個記憶體位置，所以改變 b 的值，a 也會跟著改變
 b[0] = 2
 print(a, b)
 
 a = [1, 2, 3]
-b = a.copy() # 複製 a 的值給 b，但是 b 跟 a 指向不同的記憶體位置
+b = a.copy()  # 複製 a 的值給 b，但是 b 跟 a 指向不同的記憶體位置
 b[0] = 2
 print(a, b)
 
 # list 的 append，也就是在 list 的最後面加上新的元素
 
 L = [1, 2, 3]
-L.append(4) # 把 4 加到 L 的最後面
+L.append(4)  # 把 4 加到 L 的最後面
 print(L)
 
 # list 的移除元素方式有兩種
@@ -86,26 +86,26 @@ print(L)
 # 1. 使用 remove，可以移除指定的元素
 
 L = ["a", "b", "c", "d", "a"]
-L.remove("a") # 移除第一個"a"
+L.remove("a")  # 移除第一個"a"
 
 # 代表 remove 會從頭開始找，找到第一個符合的元素就會移除
 
 # 如果想要移除所有符合的元素，可以使用迴圈
 
 for i in L:
-if i == "a":
-L.remove(i)
+    if i == "a":
+        L.remove(i)
 
 # 2. 使用 pop，可以移除指定的 index 的元素
 
 L = ["a", "b", "c", "d", "a"]
-L.pop(0) # 移除 index 0 的元素
+L.pop(0)  # 移除 index 0 的元素
 
 # 代表 pop 會移除指定的 index 的元素
 
 # 如果不指定 index，則會移除最後一個元素
 
-L.pop() # 移除最後一個元素
+L.pop()  # 移除最後一個元素
 print(L)
 
 # sort：將 List 中的元素進行排序，預設是由小到大（升序排列）
@@ -137,19 +137,19 @@ print(L)
 # 相對路徑 pages/class1-1.py
 
 f = open("pages/class1-1.py", "r", encoding="utf-8")
-content = f.read() # 讀取內容
-print(content) # 印出內容
-f.close() # 關閉檔案
+content = f.read()  # 讀取內容
+print(content)  # 印出內容
+f.close()  # 關閉檔案
 ######################################
 with open("pages/class1-1.py", "r", encoding="utf-8") as f:
-content = f.read()
-print(content) # 印出內容
+    content = f.read()
+print(content)  # 印出內容
 
 # 不用寫 f.close()，with 幫你自動關好
 
 # 字串的小工具，用來檢查字串最後是不是某個結尾
 
 filename = "class1.md"
-print(filename.endswith(".md")) # True
+print(filename.endswith(".md"))  # True
 filename2 = "notes.txt"
-print(filename2.endswith(".md")) # False
+print(filename2.endswith(".md"))  # False
